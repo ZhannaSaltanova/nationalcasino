a693ac72fe705f57807f8411c5e248f6e7716aeb
ZhannaSaltanova <nedikovazhanna@gmail.com>
2024-03-14 10:43:02 +0200

    fix

---
+++ script/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.burger-button').on('click', function(e) {
+      e.preventDefault();  
+      $('.burgermenu').toggleClass('burgermenu_active');
+    });
+  });
